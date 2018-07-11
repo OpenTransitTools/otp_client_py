a983e110b604f825bbcb18de02f4dfb70294fd30
Frank Purcell <fxpurcell@gmail.com>
2018-07-11 00:16:24 -0700

    initial add

---
+++ ott/otp_client/transit_index/routes.py
@@ -0,0 +1,19 @@
+from ott.utils import json_utils
+
+import urllib
+import logging
+log = logging.getLogger(__file__)
+
+
+class Base(object):
+    def __init__(self):
+        pass
+
+
+def main():
+    argv = sys.argv
+    print(plan)
+
+
+if __name__ == '__main__':
+    main()
