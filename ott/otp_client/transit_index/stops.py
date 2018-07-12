1fc00d5f1aa75a65f693f9e72bfe0e8f1648d30a
Frank Purcell <fxpurcell@gmail.com>
2018-07-12 16:52:54 -0700

    add Stops attribs

--- ott/otp_client/transit_index/stops.py
+++ ott/otp_client/transit_index/stops.py
@@ -9,33 +9,35 @@ log = logging.getLogger(__file__)
 class Stops(Base):
     """
     https://<domain & port>/otp/routers/default/index/routes
+    https://<domain & port>/otp/routers/default/index/stops?
+      minLat=45.50854243338104&maxLat=45.519789433696744&minLon=-122.6960849761963&maxLon=-122.65591621398927
     [
         {
-            "id": "TriMet:54",
-            "agencyName": "TriMet",
-            "shortName": "54",
-            "longName": "Beaverton-Hillsdale Hwy",
-            "mode": "BUS"
-        },
-        {
-
-            "id": "TriMet:193",
-            "longName": "Portland Streetcar - NS Line",
-            "mode": "TRAM" or "GONDOLA" or "RAIL" or ...
-            "color": "84BD00",
-            "agencyName": "Portland Streetcar"
-
+            id: TriMet:4026
+            code: 4026
+            name: SE Morrison & 9th
+            lat: 45.517303
+            lon: -122.656536
+            url: http://trimet.org/#tracker/stop/4026
         }
     ]
     """
-    longName = "LONG NAME"
+    code = "CODE (stop id)"
+    name = "STOP NAME"
+    lat = "STOP LAT"
+    lon = "STOP LON"
+    url = "STOP URL"
+
     mode = "RAIL"
 
     def __init__(self, args={}):
         super(Stops, self).__init__(args)
-        object_utils.safe_set_from_dict(self, 'longName', args)
-        object_utils.safe_set_from_dict(self, 'mode', args)
-        object_utils.safe_set_from_dict(self, 'color', args, always_cpy=False)
+        object_utils.safe_set_from_dict(self, 'code', args)
+        object_utils.safe_set_from_dict(self, 'name', args)
+        object_utils.safe_set_from_dict(self, 'lat', args)
+        object_utils.safe_set_from_dict(self, 'lon', args)
+        object_utils.safe_set_from_dict(self, 'url', args)
+        object_utils.safe_set_from_dict(self, 'mode', args, def_val="BUS")
 
     @classmethod
     def factory(cls):
